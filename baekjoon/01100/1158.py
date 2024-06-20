fe7a817e07ffe497a748b49fba8bf32eb9ff609a
koosco <rnxogud123@naver.com>
2024-06-20 10:35:56 +0900

    [baekjoon] S4:1158 deque

---
+++ baekjoon/01100/1158.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque([x for x in range(1, n+1)])
+res = []
+while n:
+    q.rotate(-k+1)
+    res.append(str(q.popleft()))
+    n -= 1
+print('<', ', '.join(res), '>', sep='')

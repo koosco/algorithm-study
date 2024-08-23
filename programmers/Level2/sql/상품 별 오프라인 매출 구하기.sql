717174dc2061dca47cdf1fcf34a74eb4f782d27d
koosco <rnxogud123@naver.com>
2024-08-24 00:53:30 +0900

    [programmers] 131533 sql

---
+++ programmers/Level2/sql/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,5 @@
+select p.product_code, sum(p.price * o.sales_amount) as sales from product p
+  join offline_sale o
+    on p.product_id = o.product_id
+ group by p.product_id
+ order by sales desc, p.product_code;
